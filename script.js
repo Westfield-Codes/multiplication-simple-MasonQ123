2a74db845532687dbd14c489b2c04f423a513ac0
Mason Quinn <mason117167@student-schoolsofwestfield.org>
2025-10-28 13:14:57 +0000

    restart

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let high = 9;
 let mistakes = [];
 
 
+
 // Main function
 function main() {
   let questions = setUp();
@@ -85,6 +86,66 @@ function showStats(score, questions) {
       showErrors(mistakes);
     }
   }
+=======
+/* Global Variables 
+ * Set default values for low and high factors
+ * Initialize mistakes array
+ */
+
+
+/* Function main() THIS REPLACES askQuestions(
+ * Calls setup to change default values
+ * Calls askQuestion with question number argument
+ * Keeps score based on returned values from askQuestion
+ * Calls showStats, which calls showTable 
+ * @param: none
+ * @return: none
+ */
+
+
+/* Function setUp()
+ * Asks if user wants to keep defaults for low, high, questions. 
+ * If not default, calls changeVar to prompt user to provide new values
+ * @param: none
+ * @return: {integer} questions
+ */
+
+
+
+/* Function changeVar(variable)
+ * Asks user which value to change variable to
+ * Parses value as integer
+ * @param: variable
+ * @return: {integer} value 
+ */
+
+
+/* Function askQuestion(question) 
+ * Asks a multiplication question: 2 factors between low and high ranges
+ * Provides feedback (correct?), returns true if correct, false if not * Adds missed factors to mistakes array.
+ * @param: {integer} question 
+ * @return: boolean value 
+ */
+
+
+/* Function showStats()
+ * Provides feedback on total correct out of total asked. 
+ * If perfect score, displays "Perfection Badge"
+ * If not perfect, displays how many right out of questions
+ * Calls showErrors to display which factors led to errors to inform study.
+ * While user wants to study tables, calls showTable for a factor.
+ * Prompt changes from "any tables" to "more tables" 
+ * @param: score, questions
+ * @return: none
+ */
+
+/* Function showErrors(errors)
+ * Provides feedback on errors by showing pairs of factors.
+ * statsAnalysis() shows most frequent factor in errors.  
+ * @param: errors
+ * @return: none
+ */
+>>>>>>> 97975a9 (restart)
 
 
   let more = confirm("Would you like to study any tables?");
